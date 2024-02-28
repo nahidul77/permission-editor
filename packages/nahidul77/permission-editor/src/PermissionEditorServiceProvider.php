e9bab725cd03d5dc51df3318a37c8d50ee2e8439
Nahidul Islam <nahiditclanbd@gmail.com>
2024-02-28 15:00:50 +0600

    Add Package skeleton

---
+++ packages/nahidul77/permission-editor/src/PermissionEditorServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Nahidul77\PermissionEditor;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionEditorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
